1b12f9e602f2f4d468a634159ec50afbe479c7aa
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-03 08:18:17 +0900

    W

---
+++ a.bat
@@ -0,0 +1,5 @@
+C:\WORKS\a p
+REM C:\WORKS\PROGRAMS\msysgit_full_1.7.11\Git\cmd\gitk
+REM C:\WORKS\a p
+
+pause
---
+++ start_prompt(muse).bat
@@ -0,0 +1,3 @@
+start C:\WORKS\WS\WS_MuseScore\"Command Prompt.lnk"
+C:\WORKS\PROGRAMS\msysgit_full_1.7.11\Git\cmd\gitk
+REM pause
